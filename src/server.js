fffac862bfff3f1eede1c40eecbbca3937329568
Martín Javier <martinjavierd@gmail.com>
2022-04-02 15:24:24 -0300

    tercer commit

--- src/server.js
+++ src/server.js
@@ -4,43 +4,55 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let carrito = 
+[
+    {
+        "id": 1,
+        "timestamp": "",
+        "producto": {
+            "id": 0,
+            "timestamp": "",
+            "nombre": "",
+            "descripcion": "",
+            "codigo": "",
+            "foto": "",
+            "precio": 0,
+            "stock": 0
+        }
+    }
+]
+
 let productos = 
 [
     {
-    "title": "Escuadra",
-    "price": 323.45,
-    "thumbnail": "./escuadra.png",
-    "id": 1
+        "id" : 1,
+        "timestamp": "",
+        "nombre": "Escuadra",
+        "descripcion": "Escuadra de madera",
+        "codigo": "001",
+        "foto": "./escuadra.png",
+        "precio": 323.45,    
+        "stock": 100
     },
     {
-    "title": "Calculadora",
-    "price": 234.56,
-    "thumbnail": "./calculadora.png",
-    "id": 2
+        "id" : 2,
+        "timestamp": "",
+        "nombre": "Calculadora",
+        "descripcion": "Calculadora Casio",
+        "codigo": "002",
+        "foto": "./calculadora.png",
+        "precio": 234.56,    
+        "stock": 102
     },
     {
-    "title": "Globo Terráqueo",
-    "price": 45.67,
-    "thumbnail": "./globoterraqueo.png",
-    "id": 3
-    },
-    {
-    "title": "Paleta Pintura",
-    "price": 456.78,
-    "thumbnail": "./paletapintura.png",
-    "id": 4
-    },
-    {
-    "title": "Reloj Reloj Reloj",
-    "price": 67.89,
-    "thumbnail": "./reloj.png",
-    "id": 5
-    },
-    {
-    "title": "Agenda Agenda",
-    "price": 78.9,
-    "thumbnail": "./agenda.png",
-    "id": 6
+        "id": 3,
+        "timestamp": "",
+        "nombre": "Globo Terráqueo",
+        "descripcion": "Globo terráqueo de madera",
+        "codigo": "003",
+        "foto": "./globoterraqueo.png",
+        "precio": 45.67,    
+        "stock": 103
     }
 ];
 
@@ -91,6 +103,56 @@ routerProductos.delete('/:id', (req, res) => {
 })
 
 app.use('/api/productos', routerProductos);
+
+const routerCarrito = express.Router();
+
+routerCarrito.post('/', (req, res) => {
+    const {body} = req
+    carrito.push(body);
+    res.status(200).send('Carrito creado');
+})
+
+routerCarrito.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    let content = carrito;
+    content = content.filter(x => {
+        return x.id != id;
+      })
+    carrito = content;
+    res.status(200).send(`Carrito ${id} eliminado`);
+})
+
+routerCarrito.get('/:id/productos', (req, res) => {
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(carrito));
+})
+
+routerProductos.post('/:id/productos', (req, res) => {
+    const {id} = req.params;
+    const {body} = req
+    let itemSearched = productos[id];
+    let content = productos;
+    content = content.filter(x => {
+        return x.id != id;
+      })
+    productos = content;
+    carrito.push(body);
+    res.status(200).send('Producto agregado al carrito');
+})
+
+routerCarrito.delete('/:id/productos/:id_prod', (req, res) => {
+    const {id} = req.params;
+    let content = carrito;
+    content = content.filter(x => {
+        return x.id != id;
+      })
+    carrito = content;
+    res.status(200).send(`Carrito ${id} eliminado`);
+})
+
+app.use('/api/carrito', routerCarrito);
+
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => { console.log(`🔥 Server started on localhost on http://localhost:${PORT}`)});
